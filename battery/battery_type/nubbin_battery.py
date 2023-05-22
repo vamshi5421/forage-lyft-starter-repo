ffefd39077b762c1e7122c0b670e9fc80d9a2476
vamshi5421 <75656830+vamshi5421@users.noreply.github.com>
2023-05-22 23:14:23 +0530

    task3

--- battery/battery_type/nubbin_battery.py
+++ battery/battery_type/nubbin_battery.py
@@ -1,15 +1,15 @@
 from battery.battery import Battery
-from utils import add_years_to_date
 
 
-class SpindlerBattery(Battery):
+class NubbinBattery(Battery):
     def __init__(self, current_date, last_service_date):
         self.current_date = current_date
         self.last_service_date = last_service_date
+        self.nubbin_year_need_service = 3
 
     def needs_service(self):
-        date_which_battery_should_be_serviced_by = add_years_to_date(self.last_service_date, 2)
-        if date_which_battery_should_be_serviced_by < self.current_date:
+        date_need_service = self.last_service_date.replace(year=self.last_service_date.year + self.nubbin_year_need_service)
+        if date_need_service < self.current_date:
             return True
         else:
             return False
--- battery/battery_type/spindler_battery.py
+++ battery/battery_type/spindler_battery.py
@@ -1,15 +1,15 @@
 from battery.battery import Battery
-from utils import add_years_to_date
 
 
-class NubbinBattery(Battery):
+class SpindlerBattery(Battery):
     def __init__(self, current_date, last_service_date):
         self.current_date = current_date
         self.last_service_date = last_service_date
+        self.spindler_year_need_service = 4
 
     def needs_service(self):
-        date_which_battery_should_be_serviced_by = add_years_to_date(self.last_service_date, 4)
-        if date_which_battery_should_be_serviced_by < self.current_date:
+        date_need_service = self.last_service_date.replace(year=self.last_service_date.year + self.spinder_year_need_service)
+        if date_need_service < self.current_date:
             return True
         else:
             return False
